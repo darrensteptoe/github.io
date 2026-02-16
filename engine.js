04f705806fd52382944d638b9f4fae05bb99cf38
darrensteptoe <me@darrensteptoe.com>
2026-02-16 03:03:36 -0600

    Update engine.js

--- engine.js
+++ engine.js
@@ -9,13 +9,28 @@ function clamp(value, min, max) {
   return Math.max(min, Math.min(max, value));
 }
 
-function calculateDeterministic(inputs) {
+function applyStructuralAdjustments(inputs) {
+  const adjusted = { ...inputs };
+
+  if (adjusted.useMovableShare) {
+    const ms = clamp(adjusted.movableShare, 0, 100);
+    adjusted.effectivePersuasionUniverse = adjusted.persuasionUniverse * (ms / 100);
+  } else {
+    adjusted.effectivePersuasionUniverse = adjusted.persuasionUniverse;
+  }
+
+  return adjusted;
+}
+
+function calculateDeterministic(inputsRaw) {
+  const inputs = applyStructuralAdjustments(inputsRaw);
+
   const projectedTurnout = inputs.totalVoters * (inputs.turnoutRate / 100);
   const winNumber = Math.floor(projectedTurnout / 2) + 1;
   const bufferedWin = Math.floor(winNumber * (1 + inputs.buffer / 100));
 
   const persuasionYield =
-    inputs.persuasionUniverse *
+    inputs.effectivePersuasionUniverse *
     (inputs.contactRate / 100) *
     (inputs.persuasionRate / 100);
 
@@ -36,11 +51,14 @@ function calculateDeterministic(inputs) {
     persuasionYield,
     gotvYield,
     totalCapacity,
-    votesNeeded
+    votesNeeded,
+    effectivePersuasionUniverse: inputs.effectivePersuasionUniverse
   };
 }
 
-function runMonteCarlo(inputs, simulations = 8000) {
+function runMonteCarlo(inputsRaw, simulations = 8000) {
+  const inputs = applyStructuralAdjustments(inputsRaw);
+
   let wins = 0;
   let margins = [];
   let losses = [];
@@ -67,17 +85,12 @@ function runMonteCarlo(inputs, simulations = 8000) {
       0, 100
     );
 
-    const simulatedTurnout =
-      inputs.totalVoters * (turnoutRate / 100);
-
-    const winNumber =
-      Math.floor(simulatedTurnout / 2) + 1;
-
-    const bufferedWin =
-      Math.floor(winNumber * (1 + inputs.buffer / 100));
+    const simulatedTurnout = inputs.totalVoters * (turnoutRate / 100);
+    const winNumber = Math.floor(simulatedTurnout / 2) + 1;
+    const bufferedWin = Math.floor(winNumber * (1 + inputs.buffer / 100));
 
     const persuasionYield =
-      inputs.persuasionUniverse *
+      inputs.effectivePersuasionUniverse *
       (contactRate / 100) *
       (persuasionRate / 100);
 
@@ -101,41 +114,32 @@ function runMonteCarlo(inputs, simulations = 8000) {
   margins.sort((a, b) => a - b);
 
   const winProbability = (wins / simulations) * 100;
+  const meanMargin = margins.reduce((a, b) => a + b, 0) / simulations;
 
-  const meanMargin =
-    margins.reduce((a, b) => a + b, 0) / simulations;
+  const worstCase = margins[Math.floor(simulations * 0.05)];
+  const bestCase  = margins[Math.floor(simulations * 0.95)];
 
-  const percentile5 =
-    margins[Math.floor(simulations * 0.05)];
-
-  const percentile95 =
-    margins[Math.floor(simulations * 0.95)];
-
-  const variance =
-    margins.reduce((sum, m) => sum + Math.pow(m - meanMargin, 2), 0) / simulations;
-
+  const variance = margins.reduce((sum, m) => sum + Math.pow(m - meanMargin, 2), 0) / simulations;
   const stdDevMargin = Math.sqrt(variance);
 
   const averageLoss =
-    losses.length > 0
-      ? losses.reduce((a, b) => a + b, 0) / losses.length
-      : 0;
+    losses.length > 0 ? losses.reduce((a, b) => a + b, 0) / losses.length : 0;
 
   return {
     winProbability,
     meanMargin,
-    worstCase: percentile5,
-    bestCase: percentile95,
+    worstCase,
+    bestCase,
     stdDevMargin,
     averageLoss
   };
 }
 
-function sensitivityAnalysis(inputs, baseProbability) {
+function sensitivityAnalysis(inputsRaw, baseProbability) {
   const bump = 1;
 
   function test(field) {
-    const modified = { ...inputs };
+    const modified = { ...inputsRaw };
     modified[field] += bump;
     return runMonteCarlo(modified, 3000).winProbability;
   }
